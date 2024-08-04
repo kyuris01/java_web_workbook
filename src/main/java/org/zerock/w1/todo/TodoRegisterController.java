64b6d32c2c77e2270294af45949c4f4978901fe3
Chris Ahn <chrisahn1007@gmail.com>
2024-08-04 12:50:52 +0900

    8/4 12:50

---
+++ src/main/java/org/zerock/w1/todo/TodoRegisterController.java
@@ -0,0 +1,22 @@
+package org.zerock.w1.todo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "todoRegisterController", urlPatterns = "/todo/register")
+public class TodoRegisterController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("입력화면을 볼 수 있도록 구성");
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("입력을 처리하고 목록 페이지로 이동");
+    }
+}
---
+++ src/main/webapp/WEB-INF/todo/list.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: window10
+  Date: 2024-08-04
+  Time: 오전 11:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>List Page</h1>
+</body>
+</html>
